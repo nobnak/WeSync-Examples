3e0e418d0b8ed42c2615c87a3cc9ac20ef67241e
Nakata Nobuyuki <nobu.nakata@gmail.com>
2023-07-10 16:11:11 +0900

    依存を切り分けてベースクラスを利用

--- Examples/WeSyncExhibitor.cs
+++ Examples/WeSyncExhibitor.cs
@@ -11,85 +11,17 @@ using UnityEngine.Events;
 namespace WeSyncSys {
 
 	[ExecuteAlways]
-	public class WeSyncExhibitor : AbstractExhibitor, IWeSync {
-
-		[SerializeField]
-		protected Camera targetCamera = null;
-		[SerializeField]
-		protected Events events = new Events();
-		[SerializeField]
-		protected Tuner tuner = new Tuner();
-
-		protected WeSpace space = new WeSpace();
-		protected WeTime time = new WeTime();
-		protected WeProjection proj = new WeProjection();
+	public class WeSyncExhibitor : WeSyncBase {
 
 		protected BaseView view;
-		protected CameraData screen;
-		protected Validator validator = new Validator();
-
-		#region unity
-		private void OnEnable() {
-			screen = default;
-			validator.Reset();
-			validator.SetCheckers(() => screen.Equals(targetCamera));
-			validator.Validation += () => {
-				screen = targetCamera;
-				if (targetCamera == null)
-					return;
-
-				proj.Apply(screen);
-
-				var uv = tuner.localUv;
-				var local = new Rect(uv.x, uv.y, uv.z, uv.w);
-				var rspace = space.Apply(screen.screenSize, local, tuner.globalSize);
-
-				targetCamera.orthographicSize = Mathf.Max(1f, 0.5f * rspace.localField.height);
-			};
-			validator.Validated += () => {
-				Notify();
-			};
-			Notify();
-		}
-		private void OnValidate() {
-			validator.Invalidate();
-		}
-		private void Update() {
-			validator.Validate();
-			space.Update();
-			time.Update();
-		}
-		#endregion
-
-		#region IWeSync
-		public WeSpace Space => space;
-		public WeTime Time => time;
-		public WeProjection Proj => proj;
-		#endregion
-
-		#region IReadonlySubspace
-		public Structures.SubSpace CurrSubspace {
-			get {
-				validator.Validate();
-				return space.CurrSubspace;
-			}
-		}
-		#endregion
 
 		#region Exhibitor
-		public override void DeserializeFromJson(string json) {
-			JsonUtility.FromJsonOverwrite(json, tuner);
-			validator.Invalidate();
-		}
-		public override object RawData() {
-			return tuner;
-		}
-		public override string SerializeToJson() {
-			validator.Validate();
-			return JsonUtility.ToJson(tuner);
-		}
 		public override void Draw() {
-			GetView().Draw();
+			if (view == null) {
+				var f = new SimpleViewFactory();
+				view = ClassConfigurator.GenerateClassView(new BaseValue<object>(tuner), f);
+			}
+			view.Draw();
 		}
 		public override void ResetView() {
 			if (view != null) {
@@ -103,38 +35,5 @@ namespace WeSyncSys {
 		public override void ResetViewModelFromModel() {
 		}
 		#endregion
-
-		#region listeners
-		public void ListenCamera(GameObject gameObject) {
-			targetCamera = gameObject.GetComponent<Camera>();
-			validator.Invalidate();
-		}
-		#endregion
-
-		#region member
-		private void Notify() => events.Changed?.Invoke(this);
-		protected virtual BaseView GetView() {
-			if (view == null) {
-				var f = new SimpleViewFactory();
-				view = ClassConfigurator.GenerateClassView(new BaseValue<object>(tuner), f);
-			}
-			return view;
-		}
-#endregion
-
-#region definition
-		[System.Serializable]
-		public class Tuner {
-			public float globalSize = 10f;
-			public Vector4 localUv = new Vector4(0f, 0f, 1f, 1f);
-		}
-		[System.Serializable]
-		public class Events {
-			[System.Serializable]
-			public class WeSyncEvent : UnityEvent<WeSyncExhibitor> { }
-
-			public WeSyncEvent Changed = new WeSyncEvent();
-		}
-#endregion
 	}
 }
