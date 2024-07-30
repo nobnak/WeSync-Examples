5ed17c3b71eef15a77677875158c8dfd38078b04
Nakata Nobu <nobu.nakata@gmail.com>
2024-07-30 17:40:20 +0900

    wesyncをupm版へ

--- Examples/WeSyncExhibitor.cs
+++ Examples/WeSyncExhibitor.cs
@@ -11,9 +11,22 @@ using UnityEngine.Events;
 namespace WeSyncSys {
 
 	[ExecuteAlways]
-	public class WeSyncExhibitor : WeSyncBase {
+	public class WeSyncExhibitor : AbstractExhibitor {
 
+		[SerializeField] protected Links links = new();
+
+		protected WeSyncBase.Tuner tuner = new();
 		protected BaseView view;
+
+		#region unity
+		private void OnEnable() {
+			var we = links.we;
+			if (we == null) {
+				Debug.LogWarning($"We sync not found");
+			}
+			ReflectChangeOf(MVVMComponent.Model);
+		}
+		#endregion
 
 		#region Exhibitor
 		public override void Draw() {
@@ -30,9 +43,34 @@ namespace WeSyncSys {
 			}
 		}
 		public override void ApplyViewModelToModel() {
-			validator.Invalidate();
+			var we = links.we;
+			if (we != null) {
+				we.CurrTuner = tuner;
+			}
 		}
 		public override void ResetViewModelFromModel() {
+			var we = links.we;
+			if (we != null) {
+				tuner = we.CurrTuner;
+			}
+		}
+
+		public override string SerializeToJson() {
+			return JsonUtility.ToJson(tuner, true);
+		}
+
+		public override void DeserializeFromJson(string json) {
+			JsonUtility.FromJsonOverwrite(json, tuner);
+			ReflectChangeOf(MVVMComponent.ViewModel);
+		}
+
+		public override object RawData() => tuner;
+		#endregion
+
+		#region declarations
+		[System.Serializable]
+		public class Links {
+			public WeSyncBase we;
 		}
 		#endregion
 	}
